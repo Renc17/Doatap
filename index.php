4b3d336af3691acfc7a9513bd9a56c0224d28176
Thodoris-P <thodoris99@hotmail.com>
2021-12-19 20:22:57 +0200

    scripts and css in different files. removed wrong jquery import

--- index.php
+++ index.php
@@ -3,18 +3,27 @@
 <head>
     <title>ΔΟΑΤΑΠ</title>
     <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
-    <script src="node_modules/bootstrap/dist/js/jquery.min.js"></script>
+    <link rel="stylesheet" href="style.css">
     <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="scripts.js"></script>
 </head>
 
 <body>
     <?php include("navbar.php"); ?>
 
     <div class="container-fluid" style="height:100vh;">
-            <div class="alert alert-danger d-flex" style="border-radius:1rem; width:40%; margin:auto;" role="alert">
+        <div id="eonly" class="alert-box">
+            <div class="alert alert-danger alert-container d-flex" role="alert">
                 <p class="col-11">Το Πρωτόκολλο του ΔΟΑΤΑΠ δέχεται έγγραφα ΜΟΝΟ δια ηλεκτρονικού ταχυδρομείου (protocol@doatap.gr), ΕΛΤΑ και ταχυμεταφορών.</p>
-                <button type="button" class="btn-close col-1 justify-content-end" aria-label="Close"></button>
+                <button type="button" class="btn-close col-1 justify-content-end" aria-label="Close" onclick="hideElement('eonly')"></button>
             </div>
+        </div>
+        <div id="isonomia" class="alert-box">
+            <div class="alert alert-danger alert-container d-flex" role="alert">
+                <p class="col-11">Για λόγους ισονομίας και ίσης μεταχείρισης των αιτόυντων, η σειρά προτεραιότητας δεν παραβιάζεται. Η διοίκηση απορρίπτει Αιτήματα υπέρβασης σειράς προτεραιότητας με το παραπάνω σκεπτικό. Μόνον όταν υπάρχει πέραν του συνήθους αδικαιολόγητη καθυστέρηση εκ μέρους του οργανισμού δίνεται έγκριση επίσπευσης </p>
+                <button type="button" class="btn-close col-1 justify-content-end" aria-label="Close" onclick="hideElement('isonomia')"></button>
+            </div>
+        </div>
     </div>
 </body>
 
---
+++ scripts.js
@@ -0,0 +1,4 @@
+function hideElement(id) {
+    var x = document.getElementById(id);
+    x.style.display = "none";
+}
