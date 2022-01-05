9c65cf1cdf7035f04765c0c9f53637eeaecb3568
Renc17 <cangaprogram@gmail.com>
2022-01-05 21:30:36 +0200

    rename class, add: variables

--- src/controllers/forms.php
+++ src/controllers/forms.php
@@ -2,9 +2,44 @@
 include 'database/database.php';
 include 'helpers/validation/create-form.php';
 
-class Forms{
+class FormController{
     private static $table = 'forms';
     private $db = null;
+
+    private $errors = array();
+
+    private $name = ''; 
+    private $surname = '';
+    private $gender = '';
+    private $father_name = '';
+    private $mother_name = '';
+    private $amka = '';
+    private $afm = '';
+    private $birth_country = '';
+    private $birth_city = '';
+    private $birth_date = '';
+    private $identification = '';
+    private $ID_num = '';
+    private $release_date = '';
+    private $release_country = '';
+    private $living_country = '';
+    private $living_city = '';
+    private $living_area = '';
+    private $address = '';
+    private $cel = '';
+    private $email = '';
+    private $diploma_type = '';
+    private $study_type = '';
+    private $diploma_recognition = '';
+    private $evaluation = '';
+    private $study_process = '';
+    private $study_country = '';
+    private $university = '';
+    private $department = '';
+    private $credits = '';
+    private $start_date = '';
+    private $deploma_date = '';
+
 
     function __construct()
     {
@@ -26,6 +61,12 @@ class Forms{
                 header('location: profile.php');
             }
         }
+    }
+
+    function getErrors($field){
+        if(isset($this->errors[$field]))
+            return $this->errors[$field];
+        return null;
     }
 
 
